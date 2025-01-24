dc50d1a3dd24a0c85a65da9b4719b2ef600810da
stanislav539261 <stanislav924114@protonmail.com>
2025-01-24 16:57:54 +0300

    LightEnvironment settings to menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,4 +1,6 @@
 #include "imgui.h"
+
+#include "light.hpp"
 #include "render.hpp"
 #include "menu.hpp"
 
@@ -13,5 +15,7 @@ Menu::~Menu() {
 void Menu::Show() {
     ImGui::Begin("Menu");
     ImGui::Checkbox("Enable Reverse Z", &g_Render->m_EnableReverseZ);
+    ImGui::SliderFloat("LightEnvironment Pitch", &g_LightEnvironment->m_Pitch, 0.f, 360.f);
+    ImGui::SliderFloat("LightEnvironment Yaw", &g_LightEnvironment->m_Yaw, 0.f, 360.f);
     ImGui::End();
 }
